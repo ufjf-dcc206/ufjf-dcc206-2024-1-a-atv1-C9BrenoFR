40d6548415cbe641110d96d86239da435b684706
Breno Furtado Rosado <breno.rosado@estudante.ufjf.br>
2024-03-26 22:41:25 -0300

    Faz teste com a div

---
+++ counter.test.js
@@ -0,0 +1,7 @@
+import {test, expect} from 'vitest';
+
+test('teste de div', ()=>{
+    const div = document.createElement('div');
+    div.textContent = "Hello, World!";
+    expect(div).toHaveTextContent("Hello, World!");
+});
